656035f53bc2b0ea011ec342059a9b10855077f1
Ravi Shekhar <ravishekhar@Ravis-MacBook-Pro.local>
2024-01-23 19:20:05 +0530

    Ejs templating engine

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const http = require('http')
-const routes = require('./routes')
+const path = require('path');
 
-const server = http.createServer(routes)
+const express = require('express');
+const bodyParser = require('body-parser');
 
-server.listen(3000)
+const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
+app.listen(3000);
